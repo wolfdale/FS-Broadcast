d22978e5a493d4dd6d45035499a4a0b8415a298a
Prakhar <mywolfdale@gmail.com>
2016-08-13 18:13:48 +0530

    Dummy Broadcast Listner

---
+++ bcast_listner/recv_bcast.cpp
@@ -0,0 +1,74 @@
+/*
+###############################################################################
+Dummy BroadCast Listner
+
+Listen to Every Broadcast on Port 5000
+For Testing Purposes
+
+###############################################################################
+*/
+
+#include<iostream>
+#include<vector>
+#include<cstdio>
+#include<string>
+#include<thread>
+#include<dirent.h>
+#include<chrono>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<sys/types.h>
+#include<arpa/inet.h>
+#include<fcntl.h>
+#include<unistd.h>
+#include<cstdlib>
+#include<sys/wait.h>
+#include<string.h>
+
+#define SUCCESS 0
+#define FAILURE 1
+#define BCAST_PORT 5000
+
+void bcast_listner()
+{
+	int bcast_sockfd;
+	int bcast_enable = 1;
+
+	struct sockaddr_in new_addr;
+
+	bcast_sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+
+	memset(&new_addr, 0, sizeof(new_addr));
+	setsockopt(bcast_sockfd, SOL_SOCKET, SO_BROADCAST, (void *)&bcast_enable, sizeof(bcast_enable));
+	new_addr.sin_family = AF_INET;
+	new_addr.sin_port = BCAST_PORT;
+	new_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+	int bind_status = bind(bcast_sockfd, (struct sockaddr*)&new_addr, sizeof(new_addr));
+
+	while(1)
+	{
+		std::this_thread::sleep_for(std::chrono::milliseconds(5000));
+		char bcast_buffer[1024];
+		unsigned slen = sizeof(bcast_buffer);
+		recvfrom(bcast_sockfd, buf, sizeof(buf)-1, 0, (sockaddr *)&new_addr, &slen);
+		std::cout << buf << std::endl;
+	}
+}
+
+
+int main(int argc, char **argv)
+{
+	std::cout << "BroadCast Listner" << std::endl;
+	std::thread broadcaster(bcast_listner);
+	broadcaster.detach();
+	while(1){}
+	return SUCCESS;
+}
+
+
+
+
+
+
+
