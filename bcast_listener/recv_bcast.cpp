fd4b70a5cc2e9b7744b5c19c4d305dfcbc71e42d
Prakhar <mywolfdale@gmail.com>
2016-08-15 04:14:46 +0530

    Mutex Lock Guard

--- bcast_listener/recv_bcast.cpp
+++ bcast_listener/recv_bcast.cpp
@@ -24,10 +24,13 @@ For Testing Purposes
 #include<cstdlib>
 #include<sys/wait.h>
 #include<string.h>
+#include<mutex>
 
 #define SUCCESS 0
 #define FAILURE 1
 #define BCAST_PORT 5000
+
+std::mutex listner_mutex;
 
 void bcast_listener()
 {
@@ -42,16 +45,20 @@ void bcast_listener()
 	setsockopt(bcast_sockfd, SOL_SOCKET, SO_BROADCAST, (void *)&bcast_enable, sizeof(bcast_enable));
 	new_addr.sin_family = AF_INET;
 	new_addr.sin_port = BCAST_PORT;
-	new_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	new_addr.sin_addr.s_addr = htonl(INADDR_BROADCAST);
 
 	int bind_status = bind(bcast_sockfd, (struct sockaddr*)&new_addr, sizeof(new_addr));
 
 	while(1)
 	{
+		std::lock_guard<std::mutex> lock(listner_mutex);
+//		listner_mutex.lock();
 		char bcast_buffer[1024];
+		memset(bcast_buffer, 0, sizeof(bcast_buffer));
 		unsigned slen = sizeof(bcast_buffer);
 		recvfrom(bcast_sockfd, bcast_buffer, sizeof(bcast_buffer)-1, 0, (sockaddr *)&new_addr, &slen);
 		std::cout << bcast_buffer << std::endl;
+//		listner_mutex.unlock();
 	}
 }
 
