cafb0cad8eb1804b6c139acbcb878007cc29689d
Prakhar <mywolfdale@gmail.com>
2016-08-28 19:51:03 +0530

    Header Files

---
+++ include/fs_header.h
@@ -0,0 +1,23 @@
+// File Broadcaster Header
+#ifndef BCAST_HEADER
+#define BCAST_HEADER
+
+#include<iostream>
+#include<vector>
+#include<cstdio>
+#include<string>
+#include<thread>
+#include<dirent.h>
+#include<chrono>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<sys/types.h>
+#include<arpa/inet.h>
+#include<fcntl.h>
+#include<unistd.h>
+#include<cstdlib>
+#include<sys/wait.h>
+#include<string.h>
+#include<mutex>
+
+#endif
