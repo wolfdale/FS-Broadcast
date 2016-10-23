e17f638ffe60bbd346b015feffbb042a6013e4f6
Prakhar <mywolfdale@gmail.com>
2016-10-23 15:45:24 +0530

    Restructuring

--- src/svr.cpp
+++ src/svr.cpp
@@ -18,18 +18,22 @@
 #include<unordered_map>
 #include <iostream>
 #include <fstream>
+
 #define SUCCESS 0
 #define FAILURE 1
 #define MAX_SIZE 1024
 #define CLI_ID "101"
 #define TOKEN "#"
-#define BCAST_PORT 6000
-#define LISTENER_PORT 54545
+//#define BCAST_PORT 6000
+//#define LISTENER_PORT 54545
+
+#define BCAST_PORT 54545
+#define LISTENER_PORT 6000
 
 char Client_id[15]={0};
 char b_cast_time[10]={0};
 
-std::unordered_map<std::string,std::string> cluster_info;
+std::unordered_map<std::string,std::string> network_fs_info;
 
 void config_parser()
 {
@@ -95,7 +99,11 @@ void fs_bcast()
 
 	while(1)
 	{
-		std::this_thread::sleep_for(std::chrono::milliseconds(5000));
+		std::cout << "Now Broadcasting ..." << std::endl;
+		/* Broadcast Every 10 Seconds (Configurable) */
+		std::this_thread::sleep_for(std::chrono::milliseconds(10000));
+		
+		/* Get files in Directory and create a FS Packet*/
 		DIR *dir;
 		struct dirent *ent;
 		char fs_data[MAX_SIZE];
@@ -115,48 +123,65 @@ void fs_bcast()
 			}
 		}
   		closedir (dir);
+
 		std::cout << fs_data << std::endl;		
+
+		//Need to implment for loop over all Socket fd to send file info
 		sendto(bcast_sockfd, fs_data, strlen(fs_data), 0, (struct sockaddr *)&new_addr, sizeof(struct sockaddr_in)); 
+		
+		
+
+
 	}
 }
 
 
 void bcast_listener()
 {
+	/* Client Address */
+	struct sockaddr_in recv_client_addr;
+	unsigned slen = sizeof(recv_client_addr);
+	
+	/* listener Address */
 	int sockfd;
-	std::cout<<"Hello"<<std::endl;
-	struct sockaddr_in new_addr;
-	struct sockaddr_in cli_addr;
-	unsigned slen = sizeof(cli_addr);
-
+	struct sockaddr_in listener_addr;
 	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-
-	memset(&new_addr, 0, sizeof(new_addr));
-	new_addr.sin_family = AF_INET;
-	new_addr.sin_port = LISTENER_PORT;
-	new_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-
-	int bind_status = bind(sockfd, (struct sockaddr*)&new_addr, sizeof(new_addr));
+	memset(&listener_addr, 0, sizeof(listener_addr));
+	listener_addr.sin_family = AF_INET;
+	listener_addr.sin_port = LISTENER_PORT;
+	listener_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	int bind_status = bind(sockfd, (struct sockaddr*)&listener_addr, sizeof(listener_addr));
 
 	while(1)
 	{
-		char bcast_buffer[1024];
+		std::cout << "Listening ..." << std::endl;
+		char bcast_buffer[MAX_SIZE];
 		memset(bcast_buffer, 0, sizeof(bcast_buffer));
-		recvfrom(sockfd, bcast_buffer, sizeof(bcast_buffer)-1, 0, (sockaddr *)&cli_addr, &slen);
+		recvfrom(sockfd, bcast_buffer, sizeof(bcast_buffer)-1, 0, (sockaddr *)&recv_client_addr, &slen);
 		std::cout << "Listening.." <<std::endl;
 		std::cout << "GOT FS" << bcast_buffer << std::endl;
+
 	}
 }
 
 
 
+
 int main(int argc, char **argv)
 {
-	config_parser();	
+	/* Configuration Parsing */
+	//config_parser();	
+	
+	/* Start Broadcaseting Thread */
 	std::thread broadcaster(fs_bcast);
+	
+	/* Start BroadCast Listener */
 	std::thread listener(bcast_listener);
+	
+	/* Make them independent */
 	broadcaster.detach();
 	listener.detach();
+	
 	while(1){}
 	return SUCCESS;
 }
